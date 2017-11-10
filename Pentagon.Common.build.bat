139a63c93668bb7fe1c6df5c9b9f650a0a2e61ad
Michal Pokorný <michal.pokorny0@gmail.com>
2017-11-10 11:24:40 +0000

    idea for building app

--- Pentagon.Common.build.bat
+++ Pentagon.Common.build.bat
@@ -1,2 +1,21 @@
-dotnet publish -c Release src\Pentagon.Common\
-copy /Y build\Pentagon.Common\*.nupkg ..\..\NuGet\
+// change verrsion
+
+
+// running cmd cmd
+var proj = "Pentagon.Common";
+
+Process cmd = new Process();
+cmd.StartInfo.FileName = "cmd.exe";
+cmd.StartInfo.RedirectStandardInput = true;
+cmd.StartInfo.RedirectStandardOutput = true;
+cmd.StartInfo.CreateNoWindow = true;
+cmd.StartInfo.UseShellExecute = false;
+cmd.Start();
+
+cmd.StandardInput.WriteLine($"dotnet publish -c Release src\{proj}\");
+cmd.StandardInput.WriteLine($"copy /Y build\{proj}\*.nupkg ..\..\NuGet\");
+cmd.StandardInput.WriteLine($"exit");
+cmd.StandardInput.Flush();
+cmd.StandardInput.Close();
+cmd.WaitForExit();
+Console.WriteLine(cmd.StandardOutput.ReadToEnd());
