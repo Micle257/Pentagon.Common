e4ef7f76f1b7b7bed9ecf93ef5678b26e95b08ea
Michal Pokorný (NTB Lenovo) <michal.pokorny0@gmail.com>
2017-11-10 15:41:33 +0100

    updated git push command

--- Git.Push.GitHub.bat
+++ Git.Push.GitHub.bat
@@ -1,3 +1,11 @@
-git filter-branch -f --env-filter "GIT_AUTHOR_NAME='Micle257'; GIT_AUTHOR_EMAIL='imiclelol@gmail'; GIT_COMMITTER_NAME='Micle257'; GIT_COMMITTER_EMAIL='imiclelol@gmail.com';" HEAD;
+git checkout master
+git checkout -B mastertogit
+git filter-branch -f --env-filter "GIT_AUTHOR_NAME='Micle257'; GIT_AUTHOR_EMAIL='imiclelol@gmail.com'; GIT_COMMITTER_NAME='Micle257'; GIT_COMMITTER_EMAIL='imiclelol@gmail.com';" HEAD
 git push -u github
-git filter-branch -f --env-filter "GIT_AUTHOR_NAME='Michal Pokorný (NTB Lenovo)'; GIT_AUTHOR_EMAIL='michal.pokorny0@gmail'; GIT_COMMITTER_NAME='Michal Pokorný (NTB Lenovo)'; GIT_COMMITTER_EMAIL='michal.pokorny0@gmail.com';" HEAD;
+
+git checkout dev
+git checkout -B devtogit
+git filter-branch -f --env-filter "GIT_AUTHOR_NAME='Micle257'; GIT_AUTHOR_EMAIL='imiclelol@gmail.com'; GIT_COMMITTER_NAME='Micle257'; GIT_COMMITTER_EMAIL='imiclelol@gmail.com';" HEAD
+git push -u github
+
+git branch -D mastertogit && git branch -D devtogit
