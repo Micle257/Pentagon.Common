7e76725c0f5597c509d6033f2c61418dd580f010
Michal Pokorný (NTB Lenovo) <michal.pokorny0@gmail.com>
2017-11-15 18:57:12 +0100

    added IHasher to seperate file

--- src/Pentagon.Common/Security/Hasher.cs
+++ src/Pentagon.Common/Security/Hasher.cs
@@ -10,34 +10,7 @@ namespace Pentagon.Security
     using System.Linq;
     using System.Security.Cryptography;
     using System.Text;
-    using JetBrains.Annotations;
     using Registration;
-
-    /// <summary> Provides a hasher algorithm. </summary>
-    public interface IHasher 
-    {
-        /// <summary> Gets the random salt used to complicate password. </summary>
-        /// <value> The salt. </value>
-        string Salt { get; }
-
-        /// <summary> Gets the hashed password with salt. </summary>
-        /// <value> The hash. </value>
-        string Hash { get; }
-
-        /// <summary> Generates the hash and random salt for given password. </summary>
-        /// <param name="newPass"> The password. </param>
-        void GenerateHashSalt([NotNull] string newPass);
-
-        /// <summary> Generates the hash with given password and salt. </summary>
-        /// <param name="newPass"> The new pass. </param>
-        /// <param name="salt"> The salt. </param>
-        void GenerateHash([NotNull] string newPass, [NotNull] string salt);
-
-        /// <summary> Compares the this hasher with another hash value. </summary>
-        /// <param name="dbHash"> The other hash to compare. </param>
-        /// <returns> <c> true </c> if hashes are equal; otherwise <c> false </c>. </returns>
-        bool CompareHash(string dbHash) ;
-    }
 
     /// <summary> Provides a hasher algorithm (SHA512). </summary>
     [Register]
---
+++ src/Pentagon.Common/Security/IHasher.cs
@@ -0,0 +1,29 @@
+﻿namespace Pentagon.Security {
+    using JetBrains.Annotations;
+
+    /// <summary> Provides a hasher algorithm. </summary>
+    public interface IHasher 
+    {
+        /// <summary> Gets the random salt used to complicate password. </summary>
+        /// <value> The salt. </value>
+        string Salt { get; }
+
+        /// <summary> Gets the hashed password with salt. </summary>
+        /// <value> The hash. </value>
+        string Hash { get; }
+
+        /// <summary> Generates the hash and random salt for given password. </summary>
+        /// <param name="newPass"> The password. </param>
+        void GenerateHashSalt([NotNull] string newPass);
+
+        /// <summary> Generates the hash with given password and salt. </summary>
+        /// <param name="newPass"> The new pass. </param>
+        /// <param name="salt"> The salt. </param>
+        void GenerateHash([NotNull] string newPass, [NotNull] string salt);
+
+        /// <summary> Compares the this hasher with another hash value. </summary>
+        /// <param name="dbHash"> The other hash to compare. </param>
+        /// <returns> <c> true </c> if hashes are equal; otherwise <c> false </c>. </returns>
+        bool CompareHash(string dbHash) ;
+    }
+}
