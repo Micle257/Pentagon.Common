093b45682f152544432fdbee5e29a3f29061bb5d
Michal Pokorný (NTB Lenovo) <michal.pokorny0@gmail.com>
2017-11-15 18:52:31 +0100

    added observable collection interfaces

---
+++ src/Pentagon.Common/Helpers/IObservableCollection.cs
@@ -0,0 +1,15 @@
+// -----------------------------------------------------------------------
+//  <copyright file="IObservableCollection.cs">
+//   Copyright (c) Michal Pokorný. All Rights Reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Pentagon.Helpers
+{
+    using System.Collections.Generic;
+    using System.Collections.Specialized;
+
+    /// <summary> Represents an observable collection. </summary>
+    /// <typeparam name="T"> The type of an item. </typeparam>
+    public interface IObservableCollection<T> : ICollection<T>, INotifyCollectionChanged { }
+}
---
+++ src/Pentagon.Common/Helpers/IObservableReadOnlyList.cs
@@ -0,0 +1,15 @@
+// -----------------------------------------------------------------------
+//  <copyright file="IObservableReadOnlyList.cs">
+//   Copyright (c) Michal Pokorný. All Rights Reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Pentagon.Helpers
+{
+    using System.Collections.Generic;
+    using System.Collections.Specialized;
+
+    /// <summary> Represents an observable read-only collection that can be access by index. </summary>
+    /// <typeparam name="T"> The type of an item. </typeparam>
+    public interface IObservableReadOnlyList<out T> : IReadOnlyList<T>, INotifyCollectionChanged { }
+}
