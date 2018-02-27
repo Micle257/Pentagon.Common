1884757604d63ca4e4b1b29b289908d52fa9d453
Michal Pokorný (NTB Lenovo) <michal.pokorny0@gmail.com>
2018-02-27 19:49:46 +0100

    added TaskHelper.TryInvoke + test

---
+++ src/Pentagon.Common/Threading/TaskHelper.cs
@@ -0,0 +1,51 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="TaskHelper.cs">
+//   Copyright (c) Michal Pokorný. All Rights Reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Pentagon.Threading
+{
+    using System;
+    using System.Threading.Tasks;
+    using JetBrains.Annotations;
+
+    /// <summary> Provides a helper methods for <seealso cref="Task" />. </summary>
+    public class TaskHelper
+    {
+        /// <summary> Attemps invoking the function for desired limit count, if the function throws an exception. </summary>
+        /// <remarks> Method is from Microsoft docs. </remarks>
+        /// <typeparam name="T"> The type of the task result. </typeparam>
+        /// <param name="function"> The function to invoke. </param>
+        /// <param name="tryLimit"> The limit number of iterations. </param>
+        /// <param name="iterationDelayInMilliseconds"> The iteration delay in milliseconds. </param>
+        /// <returns> An asynchronous operation <see cref="Task{T}" />, which was passed in <paramref name="function" />. </returns>
+        public static async Task<T> TryInvokeAsync<T>([NotNull] Func<Task<T>> function, int tryLimit, int iterationDelayInMilliseconds = 0)
+        {
+            if (function == null)
+                throw new ArgumentNullException(nameof(function));
+
+            for (var i = 0; i < tryLimit; i++)
+            {
+                try
+                {
+                    var task = function();
+                    if (task != null)
+                       return await task.ConfigureAwait(false);
+                }
+                // ReSharper disable once CatchAllClause
+                catch
+                {
+                    if (i == tryLimit - 1)
+                        throw;
+                }
+
+                if (iterationDelayInMilliseconds > 0)
+                        // ReSharper disable once PossibleNullReferenceException
+                    await Task.Delay(iterationDelayInMilliseconds).ConfigureAwait(false);
+            }
+
+            return default;
+        }
+    }
+}
---
+++ test/Pentagon.Common.Tests/Threading/ContainerContainmentTests.cs
@@ -0,0 +1,34 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="ContainerContainmentTests.cs">
+//   Copyright (c) Michal Pokorný. All Rights Reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Pentagon.Common.Tests.Threading
+{
+    using System;
+    using System.Threading.Tasks;
+    using Pentagon.Threading;
+    using Xunit;
+
+    public class TaskHelperTests
+    {
+        [Fact]
+        public void TryInvokeAsync_RetryCountExceeded_Throws()
+        {
+            Func<Task<bool>> func = () => Task.Run((Func<bool>)(() => throw new ArgumentException()));
+
+            Assert.Throws<AggregateException>(() => TaskHelper.TryInvokeAsync(func, 5).Wait());
+        }
+
+        [Fact]
+        public void TryInvokeAsync_RetryCountIsGreatenThanFunctionFailureCount_InvokesFunction()
+        {
+            Func<Task<bool>> func = () => Task.Run(() => true);
+
+            var result = TaskHelper.TryInvokeAsync(func, 2).Result;
+
+            Assert.True(result);
+        }
+    }
+}
