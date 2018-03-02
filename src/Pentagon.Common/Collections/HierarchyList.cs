0fa657f22304f9bc725bbd368d327647e71a705e
Michal Pokorný (NTB Lenovo) <michal.pokorny0@gmail.com>
2018-03-02 15:57:29 +0100

    Added HierarchyList and HierarchyItem types

---
+++ src/Pentagon.Common/Collections/HierarchyList.cs
@@ -0,0 +1,99 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="HierarchyList.cs">
+//   Copyright (c) Michal Pokorný. All Rights Reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Pentagon.Collections
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary> Represents a collection in hierarchy structure. </summary>
+    /// <typeparam name="T"> The type of an item. </typeparam>
+    public class HierarchyList<T> : IEnumerable<T>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HierarchyList{T}"/> class.
+        /// </summary>
+        /// <param name="rootItem">The root item.</param>
+        public HierarchyList(T rootItem)
+        {
+            Root = new HierarchyListItem<T>(rootItem, null);
+        }
+
+        /// <summary> Gets the root item. </summary>
+        /// <value> The list of the <see cref="HierarchyListItem{T}" />. </value>
+        public HierarchyListItem<T> Root { get;  }
+
+        /// <inheritdoc />
+        public IEnumerator<T> GetEnumerator() => new Enumerator(Root);
+
+        /// <inheritdoc />
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        /// <summary> Represents an enumerator for <see cref="HierarchyList{T}" />. </summary>
+        class Enumerator : IEnumerator<T>
+        {
+            /// <summary>
+            /// The root item.
+            /// </summary>
+            readonly HierarchyListItem<T> _item;
+
+            /// <summary>
+            /// The current item row.
+            /// </summary>
+            List<HierarchyListItem<T>> _currentItemRow;
+
+            /// <summary>
+            /// The current index in the row.
+            /// </summary>
+            int _currentIndex;
+
+            /// <summary> Initializes a new instance of the <see cref="Enumerator" /> class. </summary>
+            /// <param name="item"> The item. </param>
+            public Enumerator(HierarchyListItem<T> item)
+            {
+                _item = item;
+            }
+
+            /// <inheritdoc />
+            public T Current { get; private set; }
+
+            /// <inheritdoc />
+            object IEnumerator.Current => Current;
+
+            /// <inheritdoc />
+            public bool MoveNext()
+            {
+                if (_currentItemRow == null)
+                    _currentItemRow = new List<HierarchyListItem<T>> {_item};
+                else if (_currentItemRow.Count == 0)
+                    return false;
+
+                Current = (_currentItemRow[_currentIndex] ?? throw new ArgumentNullException()).Value;
+
+                if (_currentIndex == _currentItemRow.Count - 1)
+                {
+                    _currentIndex = 0;
+                    _currentItemRow = _currentItemRow.SelectMany(a => a.Children).ToList();
+                }
+                else
+                    _currentIndex++;
+
+                return true;
+            }
+
+            /// <inheritdoc />
+            public void Reset()
+            {
+                Current = default;
+            }
+
+            /// <inheritdoc />
+            public void Dispose() { }
+        }
+    }
+}
---
+++ src/Pentagon.Common/Collections/HierarchyListItem.cs
@@ -0,0 +1,65 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="HierarchyListItem.cs">
+//   Copyright (c) Michal Pokorný. All Rights Reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Pentagon.Collections
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary> Represents an item in hierarchy structure. </summary>
+    /// <typeparam name="T"> </typeparam>
+    public class HierarchyListItem<T>
+    {
+        /// <summary> Initializes a new instance of the <see cref="HierarchyListItem{T}" /> class. </summary>
+        /// <param name="value"> The value. </param>
+        /// <param name="parent"> The parent. </param>
+        public HierarchyListItem(T value, HierarchyListItem<T> parent)
+        {
+            Value = value;
+            Parent = parent;
+        }
+
+        /// <summary> Gets the parent. </summary>
+        /// <value> The list of the <see cref="HierarchyListItem{T}" />. </value>
+        public HierarchyListItem<T> Parent { get; }
+
+        /// <summary> Gets the children items. </summary>
+        /// <value> The list of the <see cref="IList{T}" />. </value>
+        public IList<HierarchyListItem<T>> Children { get; } = new List<HierarchyListItem<T>>();
+
+        /// <summary> Gets the item that share the same parent. </summary>
+        /// <value> The list of the <see cref="HierarchyListItem{T}" />. </value>
+        public IList<HierarchyListItem<T>> Siblings => GetSiblings();
+
+        /// <summary> Gets a value indicating whether this item is root. </summary>
+        /// <value> <c> true </c> if this item is root; otherwise, <c> false </c>. </value>
+        public bool IsRoot => Parent == null;
+
+        /// <summary> Gets or sets the value. </summary>
+        /// <value> The <see cref="T" />. </value>
+        public T Value { get; set; }
+
+        /// <summary> Adds a children. </summary>
+        /// <param name="item"> The children. </param>
+        public void AddChildren(T item)
+        {
+            Children.Add(new HierarchyListItem<T>(item, this));
+        }
+
+        /// <summary> Gets the siblings. </summary>
+        /// <returns> A list of the <see cref="HierarchyListItem{T}" />. </returns>
+        IList<HierarchyListItem<T>> GetSiblings()
+        {
+            if (IsRoot)
+                return null;
+
+            if (Parent.Children != null && Parent.Children.Count != 0)
+                return Parent.Children.Except(new[] {this}).ToList();
+
+            return null;
+        }
+    }
+}
