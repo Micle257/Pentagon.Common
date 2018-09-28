1e60130ce6392df2bf9ea9a161e6716201765fd0
Michal Pokorný <michal.pokorny0@gmail.com>
2018-09-28 10:32:33 +0200

    small fixes

--- src/Pentagon.Common/Collections/HierarchyListItem.cs
+++ src/Pentagon.Common/Collections/HierarchyListItem.cs
@@ -8,11 +8,18 @@ namespace Pentagon.Collections
 {
     using System.Collections.Generic;
     using System.Linq;
+    using JetBrains.Annotations;
 
     /// <summary> Represents an item in hierarchy structure. </summary>
     /// <typeparam name="T"> </typeparam>
     public class HierarchyListItem<T>
     {
+        /// <summary>
+        /// The list of children.
+        /// </summary>
+        [NotNull]
+        readonly List<HierarchyListItem<T>> _children = new List<HierarchyListItem<T>>();
+
         /// <summary> Initializes a new instance of the <see cref="HierarchyListItem{T}" /> class. </summary>
         /// <param name="value"> The value. </param>
         /// <param name="parent"> The parent. </param>
@@ -28,11 +35,11 @@ namespace Pentagon.Collections
 
         /// <summary> Gets the children items. </summary>
         /// <value> The list of the <see cref="IList{T}" />. </value>
-        public IList<HierarchyListItem<T>> Children { get; } = new List<HierarchyListItem<T>>();
+        public IReadOnlyList<HierarchyListItem<T>> Children => _children;
 
         /// <summary> Gets the item that share the same parent. </summary>
         /// <value> The list of the <see cref="HierarchyListItem{T}" />. </value>
-        public IList<HierarchyListItem<T>> Siblings => GetSiblings();
+        public IReadOnlyList<HierarchyListItem<T>> Siblings => GetSiblings();
 
         /// <summary> Gets a value indicating whether this item is root. </summary>
         /// <value> <c> true </c> if this item is root; otherwise, <c> false </c>. </value>
@@ -46,14 +53,14 @@ namespace Pentagon.Collections
         /// <param name="item"> The children. </param>
         public void AddChildren(T item)
         {
-            Children.Add(new HierarchyListItem<T>(item, this));
+            _children.Add(new HierarchyListItem<T>(item, this));
         }
 
         /// <summary> Gets the siblings. </summary>
         /// <returns> A list of the <see cref="HierarchyListItem{T}" />. </returns>
-        IList<HierarchyListItem<T>> GetSiblings()
+        IReadOnlyList<HierarchyListItem<T>> GetSiblings()
         {
-            if (IsRoot)
+            if (IsRoot || Parent == null)
                 return null;
 
             if (Parent.Children != null && Parent.Children.Count != 0)
