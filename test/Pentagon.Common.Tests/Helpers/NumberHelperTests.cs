22e48a36cf15d1284a01003d95de316f0c8cca6a
Michal Pokorný <michal.pokorny0@gmail.com>
2018-10-26 16:43:24 +0200

    added NumberHelper.SwapAndWrap + tests

---
+++ test/Pentagon.Common.Tests/Helpers/NumberHelperTests.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="NumberHelperTests.cs">
+//   Copyright (c) Michal Pokorný. All Rights Reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Pentagon.Common.Tests.Helpers
+{
+    using Pentagon.Helpers;
+    using Xunit;
+
+    public class NumberHelperTests
+    {
+        [Theory]
+        [InlineData(5, 2, 20)]
+        [InlineData(5, 1, 10)]
+        [InlineData(int.MaxValue, 2, -3)]
+        [InlineData(int.MinValue, 1, 1)]
+        [InlineData(0, 15, 0)]
+        public void ShiftAndWrap_ReturnsCorrectValue(int value, int position, int expected)
+        {
+            var result = NumberHelper.ShiftAndWrap(value, position);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
