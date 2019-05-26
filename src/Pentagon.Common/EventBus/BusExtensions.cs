4f75d1255caf59beed07053e7fdd772eb8a3fd80
Michal Pokorný <michal.pokorny0@gmail.com>
2019-05-26 13:18:35 +0200

    added event bus

---
+++ src/Pentagon.Common/EventBus/BusExtensions.cs
@@ -0,0 +1,25 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="BusExtensions.cs">
+//   Copyright (c) Smartdata s. r. o. All Rights Reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Pentagon.EventBus
+{
+    using System;
+
+    public static class BusExtensions
+    {
+        public static Guid Subscribe<TMessage>(this IEventBus bus, Func<IHandle<TMessage>> handlerFactory)
+        {
+            return bus.Subscribe<TMessage>(message
+                                                   => handlerFactory.Invoke().Handle(message));
+        }
+
+        public static Guid Subscribe<TMessage>(this IEventBus bus, Func<IHandleAsync<TMessage>> handlerFactory)
+        {
+            return bus.Subscribe<TMessage>((message, cancellationToken)
+                                                   => handlerFactory.Invoke().HandleAsync(message, cancellationToken));
+        }
+    }
+}
