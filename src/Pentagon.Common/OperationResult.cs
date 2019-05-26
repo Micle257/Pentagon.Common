0ab553bd2cef418995c4004c9ecceb24a06c31d0
Michal Pokorný <michal.pokorny0@gmail.com>
2019-05-26 13:18:46 +0200

    added operation result

---
+++ src/Pentagon.Common/OperationResult.cs
@@ -0,0 +1,25 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="OperationResult.cs">
+//   Copyright (c) Michal Pokorný. All Rights Reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Pentagon
+{
+    using System;
+
+    public class OperationResult
+    {
+        public static OperationResult Success => new OperationResult();
+
+        public bool IsSuccessful => Exception == null;
+
+        public Exception Exception { get; set; }
+
+        public static OperationResult FromFailed(Exception exception) =>
+                new OperationResult
+                {
+                        Exception = exception
+                };
+    }
+}
